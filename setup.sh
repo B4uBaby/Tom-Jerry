daeb09aa6bcd78ed40c099091637f883cb7346ca
Bhart sharma <59437165+BhartSharmawhitehathacker@users.noreply.github.com>
2020-09-22 22:53:27 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#bin!bash
+
+ech " Wait 
+
+"
+
+figlet -f big set up ...
+
+apt update -y
+apt upgrade -y
+pkg install nmap
+
+clear
+
+figlet -f big Set up Done 
+
+echo "now You Can Run this tool by Typing this command "
+
+echo " bash 'tom jerry.sh'
+
+
+" 
+
+echo "Thank You 
+
+" 
